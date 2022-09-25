7ba29036db9f8fb8ecdfcc3c4c541d80b216accc
Schedir <sailingschedir@gmail.com>
2022-09-25 20:23:13 +0200

    Commit 3
    Change font to Cairo

--- .next/static/webpack/webpack.600df3f9494c490f.hot-update.js
+++ .next/static/webpack/webpack.600df3f9494c490f.hot-update.js
@@ -41,21 +41,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c0ed18986b2e2aa6"; }
+/******/ 	__webpack_require__.h = function() { return "3ec44d7294c603ba"; }
 /******/ }();
-/******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }
 /******/ 
 /******/ }
 );
