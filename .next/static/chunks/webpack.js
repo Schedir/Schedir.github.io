3f2116ace93ec680b789afcb88bdae3894290779
Schedir <sailingschedir@gmail.com>
2022-10-28 00:47:43 +0200

    Commit 20.2
    Aggiornamento pagina Schedir e Logbook. Modifiche font e margini per Mobile ed altre dimensioni di pagina.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e69fe718e38466f"; }
+/******/ 		__webpack_require__.h = function() { return "2bdcb7179e6d246b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
