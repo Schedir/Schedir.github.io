0219c99636bfd6c950ff9c6a870f539df5fd16ab
Schedir <sailingschedir@gmail.com>
2022-10-29 23:01:06 +0200

    Commit 21

---
+++ .history/pages/_app_20221029225140.js
@@ -0,0 +1,45 @@
+import '../styles/globals.css'
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+import { useRouter } from "next/router";
+import {AnimatePresence, motion} from "framer-motion";
+
+function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  return (
+    <div>
+      <NavBar/>
+        <AnimatePresence mode='wait'>
+          <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{
+            duration: 0.75,
+          }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              x:200,
+            },
+            animateState: {
+              opacity: 1,
+              x:0,
+            },
+            exitState: {
+              opacity:1,
+              x:-200,
+            },
+          }}>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      <Footer/>
+    </div>
+  );
+}
+
+export default MyApp;
