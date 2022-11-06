94e03bac65d02173ee334bafefe2c14d095f284b
Schedir <sailingschedir@gmail.com>
2022-11-06 16:25:24 +0100

    Commit 24.2
    Homepage update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ae4bcc9da9f82e5"; }
+/******/ 		__webpack_require__.h = function() { return "c6b604ffeda5a988"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
