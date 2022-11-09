60d5beecedc78ba1e0056a7dd4abda95eeb513cf
Schedir <sailingschedir@gmail.com>
2022-11-10 00:49:57 +0100

    Commit 27.2

---
+++ .history/pages/_app_20221109234044.js
@@ -0,0 +1,45 @@
+import '../styles/globals.css'
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+import { useRouter } from "next/router";
+import {AnimatePresence, motion} from "framer-motion";
+
+function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  return (
+    <div className='scroll-smooth'>
+      <NavBar/>
+        <AnimatePresence mode='wait'>
+          <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{
+            duration: 1,
+          }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              y:200
+            },
+            animateState: {
+              opacity: 1,
+              y:0
+            },
+            exitState: {
+              opacity:0,
+              y:-200
+            },
+          }}>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      <Footer/>
+    </div>
+  );
+}
+
+export default MyApp;
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a05961133ca067b5"; }
+/******/ 		__webpack_require__.h = function() { return "28fc29ea70855ba5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
