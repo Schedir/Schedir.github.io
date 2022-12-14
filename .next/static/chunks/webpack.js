aba136f2bc5b272241e86d4eb50684a19fac67bc
l.pastore6@campus.unimib.it <sailingschedir@gmail.com>
2022-12-14 11:58:40 +0100

    Commit 38

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b11886ea0146a93"; }
+/******/ 		__webpack_require__.h = function() { return "55d70aacfea9508c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
