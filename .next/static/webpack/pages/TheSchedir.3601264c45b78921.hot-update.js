a0716b85a9c67d7ea2bf21684bcee319e284b685
l.pastore6@campus.unimib.it <sailingschedir@gmail.com>
2023-01-09 00:06:53 +0100

    Commit 43.1

---
+++ .next/static/webpack/pages/TheSchedir.3601264c45b78921.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/TheSchedir",{
+
+/***/ "./components/TheSchedirComps/data.json":
+/*!**********************************************!*\
+  !*** ./components/TheSchedirComps/data.json ***!
+  \**********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"resources":[{"title":"Find me on Twitter","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/500/300/any"},{"title":"Welcome to Ark Labs","link":"https://ark-labs.co.uk","imageUrl":"https://placeimg.com/300/300/animals"},{"title":"Some sort of third title","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/architecture"},{"title":"A personal site perhaps?","link":"https://robkendal.co.uk","imageUrl":"https://placeimg.com/300/300/nature"},{"title":"Super item number five","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/people"},{"title":"Super item number six","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/tech"},{"title":"Super item number seven","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/animals"},{"title":"Super item number eight","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/people"},{"title":"Super item number the last","link":"https://twitter.com/kendalmintcode","imageUrl":"https://placeimg.com/300/300/tech"}]}');
+
+/***/ })
+
+});
